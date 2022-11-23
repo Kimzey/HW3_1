d744362eaf7b87fc0262d135cdf00dedd7c6727c
Kimzey <111524127+Kimzey@users.noreply.github.com>
2022-11-23 17:46:26 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,8 +3,10 @@ class Program
     static void HW_1(){
         CircularLinkedList<char> fower = new CircularLinkedList<char>(); 
         while(true){
+
             Console.Write("Input Fower {J,G,O,R}: ");
             char Fower_input = char.Parse(Console.ReadLine());
+
             if (Fower_input != 'J' && Fower_input != 'G' && Fower_input != 'O'&& Fower_input != 'R' ){
                 break;
             }
@@ -12,21 +14,20 @@ class Program
                 int Check_length = fower.GetLength();
 
                 if(Check_length == 0){
-                    if(Fower_input == 'R'){ //เช็ค ห้ามตัวแรกเป็น R
+                    if(Fower_input == 'R'){
                         Console.WriteLine("Invalid pattern.");
                     }
                     else{
                     fower.Add(Fower_input);
                     }
                 }
-
                 else if(Check_length >= 1){
 
                     if(Check_length <= 2){
                         if (fower.Get(-1) == 'R'){//เช็คระหว่าง R
-                            if(Fower_input == fower.Get(-2)){
+                        if(Fower_input == fower.Get(-2) || Fower_input == 'R'){
                             Console.WriteLine("Invalid pattern.");}
-                            else{
+                        else{
                             fower.Add(Fower_input);
                         }
                     }
@@ -35,14 +36,26 @@ class Program
                     }}
 
                     else if(Check_length >= 3){
+
                     if (fower.Get(-1) == 'R' && Fower_input == fower.Get(-2)){//เช็คระหว่าง R 
-                            Console.WriteLine("Invalid pattern.");
-                    }
-                    else if(Fower_input == 'G'){ // เช็ค G ห้ามเกิน 4 
-                        if(fower.Get(0) == 'G' && fower.Get(1) == 'G' && fower.Get(2) == 'G'){
+                        if (Fower_input == 'R'){
                             Console.WriteLine("Invalid pattern.");
                         }
-                        else if(Fower_input == fower.Get(-1) && Fower_input == fower.Get(-2)&& Fower_input == fower.Get(-3)){
+                        else if (Fower_input == fower.Get(-2)){
+                            Console.WriteLine("Invalid pattern.");
+                        }
+                        else{
+                            fower.Add(Fower_input);
+                        }
+                    }
+                    else if(Fower_input == 'G'){ // เช็ค G ห้ามเกิน 4
+                        if(Fower_input == fower.Get(-1) && Fower_input == fower.Get(-2)&& Fower_input == fower.Get(-3)&& Fower_input == 'G'){//เช็ค G หลัง 3 ตัว ตัวล่าสุด ห้ามเป็น G
+                            Console.WriteLine("Invalid pattern.");
+                        }
+                        else if(fower.Get(0) == 'G' && fower.Get(1) == 'G' && fower.Get(2) == 'G' && Fower_input == 'G'){ //เช็ค G ตัว 1 2 3 ตัวล่าสุด ห้าม G
+                            Console.WriteLine("Invalid pattern.");
+                        }
+                        else if (fower.Get(0) == 'G' && fower.Get(-1) == 'G' && fower.Get(-2) == 'G' && Fower_input == 'G' ){ // เช็ค G ตัวแรก และ เช็คตัวท้าย 2 ตัว ตัวล่าสุด ห้ามเป็น G 
                             Console.WriteLine("Invalid pattern.");
                         }
                         else{
@@ -52,6 +65,7 @@ class Program
                     else {
                         fower.Add(Fower_input);
                     }
+
                     }
             }
         }
